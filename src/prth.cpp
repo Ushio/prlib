e13285a7dc5916ad53232bdda5dfb9a506f05ef2
ushiostarfish <ushiostarfish@gmail.com>
2020-10-03 23:15:17 +0900

    IOCP

--- src/prth.cpp
+++ src/prth.cpp
@@ -1,7 +1,148 @@
 #include "prth.hpp"
 
+#if ENABLE_IOCP
+
+#define NOMINMAX
+#include <Windows.h>
+
+#endif
+
 namespace pr
 {
+#if ENABLE_IOCP
+    struct TaskObject
+    {
+        ThreadPool::Task task;
+    };
+
+    ThreadPool::ThreadPool(int nThreads, int64_t nReminedElement)
+        :_nThreads(nThreads), _nReminedElement(nReminedElement)
+    {
+        _iocp = (void *)CreateIoCompletionPort( INVALID_HANDLE_VALUE, 0, 0, nThreads );
+
+        _hasElement = 0 < _nReminedElement;
+        _executingCount = 0;
+
+        launchThreads(1);
+        if (1 < nThreads)
+        {
+            _initThreads = std::async(std::launch::async, [nThreads, this] {
+                launchThreads(nThreads - 1);
+            });
+        }
+    }
+    ThreadPool::~ThreadPool()
+    {
+        if (_initThreads.valid())
+        {
+            _initThreads.get();
+        }
+
+        CloseHandle((HANDLE)_iocp);
+
+        for (int i = 0; i < _workers.size(); ++i)
+        {
+            _workers[i].join();
+        }
+        _workers.clear();
+    }
+
+    void ThreadPool::enqueueTask(Task task)
+    {
+        TaskObject* o = new TaskObject();
+        o->task = std::move(task);
+        PostQueuedCompletionStatus((HANDLE)_iocp, 0, (ULONG_PTR)o, 0);
+    }
+    void ThreadPool::enqueueFor(int64_t nExecute, int64_t splitLevel, std::function<void(int64_t, int64_t, ThreadPool*)> work)
+    {
+        int nSplit = _nThreads * splitLevel; // this is really adhoc
+        int cur = 0;
+        for (int i = 0; i < nSplit; ++i)
+        {
+            int n = (nExecute + i) / nSplit;
+            int beg = cur;
+            cur += n;
+            int end = cur;
+
+            if (beg == end)
+            {
+                continue;
+            }
+
+            enqueueTask([beg, end, work](ThreadPool* p) {
+                work(beg, end, p);
+            });
+        }
+    }
+    void ThreadPool::addElements(int64_t nElements)
+    {
+        assert(0 < nElements);
+
+        int64_t prev = _nReminedElement.fetch_add(nElements);
+        if (prev == 0)
+        {
+            std::unique_lock<std::mutex> lockGuard(_statusMutex);
+            _hasElement = true;
+        }
+    }
+    void ThreadPool::doneElements(int64_t nElements)
+    {
+        assert(0 < nElements);
+
+        int64_t previous = _nReminedElement.fetch_sub(nElements);
+        if (previous == nElements)
+        {
+            {
+                std::unique_lock<std::mutex> lockGuard(_statusMutex);
+                _hasElement = false;
+            }
+            _statusCondition.notify_all();
+        }
+    }
+    void ThreadPool::waitForDoneElements()
+    {
+        std::unique_lock<std::mutex> lockGuard(_statusMutex);
+        _statusCondition.wait(lockGuard, [this] {
+            return _hasElement == false;
+        });
+    }
+
+    void ThreadPool::launchThreads(int nThreads)
+    {
+        for (int i = 0; i < nThreads; ++i)
+        {
+            _workers.emplace_back([this] {
+                for (;;)
+                {
+                    LPOVERLAPPED aux;
+                    DWORD        cmd;
+                    TaskObject* o = 0;
+                    if( GetQueuedCompletionStatus((HANDLE)_iocp, &cmd, (ULONG_PTR*)&o, &aux, INFINITE) == FALSE )
+                    {
+                        break;
+                    }
+                    _executingCount++;
+                    o->task(this);
+                    _executingCount--;
+                }
+            });
+        }
+    }
+    ThreadPool::ProcessResult ThreadPool::processTask()
+    {
+        LPOVERLAPPED aux;
+        DWORD        cmd;
+        TaskObject* o = 0;
+        if( GetQueuedCompletionStatus((HANDLE)_iocp, &cmd, (ULONG_PTR*)&o, &aux, 0) == FALSE )
+        {
+            return ProcessResult::NoTask;
+        }
+        o->task(this);
+        delete o;
+
+        return ProcessResult::Consumed;
+    }
+#else
 	ThreadPool::ThreadPool(int nThreads, int64_t nReminedElement)
         :_nThreads(nThreads), _nReminedElement(nReminedElement)
     {
@@ -116,7 +257,7 @@ namespace pr
                         std::unique_lock<std::mutex> lockGuard(_taskMutex);
                         _taskCondition.wait(lockGuard, [this] {
                             return _tasks.empty() == false || _continue == false;
-                            });
+                        });
                         if (_continue == false)
                         {
                             break;
@@ -158,5 +299,6 @@ namespace pr
 		}
         return ProcessResult::NoTask;
 	}
+#endif
 }
 
--- src/prth.hpp
+++ src/prth.hpp
@@ -10,8 +10,58 @@
 #include <mutex>
 #include <condition_variable>
 
+#define ENABLE_IOCP 1
+
 namespace pr
 {
+#if ENABLE_IOCP
+    class ThreadPool {
+    public:
+        using Task = std::function<void(ThreadPool*)>;
+
+        ThreadPool(int nThreads, int64_t nReminedElement = 0);
+        ~ThreadPool();
+        ThreadPool(const ThreadPool&) = delete;
+        ThreadPool& operator=(const ThreadPool&) = delete;
+
+        void enqueueTask(Task task);
+
+        // work will be separated into splitLevel * nThreads tasks
+        void enqueueFor(int64_t nExecute, int64_t splitLevel, std::function<void(int64_t, int64_t, ThreadPool*)> work);
+
+        // Manual task control
+        void addElements(int64_t nElements);
+        void doneElements(int64_t nElements);
+
+        void waitForDoneElements();
+
+        // process 1 task. you can use this even inside of Task function.
+        // It's useful for nested enqueueFor()
+        enum class ProcessResult
+        {
+            NoTask,
+            Consumed,
+        };
+        ProcessResult processTask();
+
+        int threadCount() const { return _nThreads; }
+        float occupancy() const { return (float)_executingCount / _nThreads; };
+    private:
+        void launchThreads(int nThreads);
+    private:
+        int _nThreads = 0;
+        std::future<void> _initThreads;
+        std::vector<std::thread> _workers;
+        void* _iocp;
+        std::atomic<int64_t> _nReminedElement;
+
+        std::mutex _statusMutex;
+        std::condition_variable _statusCondition;
+        bool _hasElement;
+
+        std::atomic<int32_t> _executingCount;
+    };
+#else
     class ThreadPool {
     public:
         using Task = std::function<void(ThreadPool*)>;
@@ -61,4 +111,5 @@ namespace pr
 
         std::atomic<int32_t> _executingCount;
     };
+#endif
 }
