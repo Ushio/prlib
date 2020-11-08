a06478771704a1c56e42104a185a85a7c334d082
Ushio <ushiostarfish@gmail.com>
2020-11-08 16:23:59 +0900

    change default toolkit

--- example_as_submodule/create_project.bat
+++ example_as_submodule/create_project.bat
@@ -3,5 +3,5 @@ git submodule add https://github.com/Ushio/prlib libs/prlib
 copy libs\prlib\premake5.exe premake5.exe
 copy libs\prlib\example_as_submodule\premake5.lua premake5.lua
 copy libs\prlib\example_as_submodule\main.cpp main.cpp
-premake5.exe vs2017
+premake5.exe vs2019
 start build\HogeProject.sln
